6dab6189978b8726f96094fe2f1ab903e0a1de2f
JaidenZ <haojun.zhao@icloud.com>
2021-08-24 17:52:07 +0800

    Init Project

---
+++ 04_Host/Venom.Host.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Venom.Host.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
