4aab2e41b4e5469154926e28b9e1b580356852e0
JaidenZ <haojun.zhao@icloud.com>
2021-09-17 18:09:11 +0800

    update:定义机器学习基础

---
+++ 03_Core/Venom.Core.Machine/Proxy/IMachineBase.cs
@@ -0,0 +1,46 @@
+﻿namespace Venom.Core.Machine.Proxy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    internal interface IMachineBase
+    {
+        string MachineName { get; }
+
+        DateTime InitialTime { get; }
+
+        abstract void InitialMachine(string machineName);
+    }
+
+
+    internal interface IMind
+    {
+
+        string MindName { get; set; }
+
+        Action<object> PreviousStep { get; set; }
+
+        Action<object> NextStep { get; set; }
+
+        IMind GetMind(string mindName);
+
+    }
+
+    internal interface IBehavior
+    {
+        string BehaviorName { get; set; }
+
+        int BehaviorType {  get; set; }
+
+        int BehaviorState { get; set; }
+
+
+
+    }
+
+}
---
+++ 03_Core/Venom.Core.Machine/Proxy/IVenom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venom.Core.Machine.Proxy
+{
+    internal interface IVenom:IMachineBase
+    {
+        /// <summary>
+        /// 修正思维
+        /// </summary>
+        /// <param name="mind"></param>
+        abstract void CorrectionMind(IMind mind);
+
+        /// <summary>
+        /// 执行行为
+        /// </summary>
+        abstract void ExecuteBehavior(IBehavior behavior);
+
+
+
+
+    }
+}
